0288196f2c8a6d11b11f61747cf4304494ad2524
TAYLUNG <alefdutran@gmail.com>
2026-02-16 16:53:53 -0300

    Componentizando a aplicação

---
+++ src/components/Formulario.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+    <form @submit.prevent="cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="tarefaTemp" @input="editaTarefaTemp" required type="text"
+                    placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="trocarFiltro" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
